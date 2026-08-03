8ee35ff5a9447f85c75a3c1c2705893705640763
putkidibosh <putkidibosh@gmail.com>
2026-08-04 02:56:41 +0600

    Update getFbstate.js

--- bot/login/getFbstate.js
+++ bot/login/getFbstate.js
@@ -1,32 +1,16 @@
-const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
 
-module.exports = async function (tokenFullPermission) {
-	const response1 = await axios({
-		url: 'https://graph.facebook.com/app',
-		method: "GET",
-		params: {
-			access_token: tokenFullPermission
+module.exports = async function () {
+	try {
+		const appStatePath = path.join(process.cwd(), "appstate.json");
+		if (fs.existsSync(appStatePath)) {
+			const appStateData = fs.readFileSync(appStatePath, "utf8");
+			return JSON.parse(appStateData);
+		} else {
+			throw new Error("appstate.json file not found in root directory!");
 		}
-	});
-	if (response1.data.error)
-		throw new Error("Token is invalid");
-
-	const response2 = await axios({
-		url: 'https://api.facebook.com/method/auth.getSessionforApp',
-		method: "GET",
-		params: {
-			access_token: tokenFullPermission,
-			format: "json",
-			new_app_id: response1.data.id,
-			generate_session_cookies: '1'
-		}
-	});
-	if (response2.data.error_code)
-		throw new Error("Token is invalid");
-	else if (response2.data.session_cookies?.length >= 0)
-		return response2.data.session_cookies.map(x => {
-			x.key = x.name;
-			delete x.name;
-			return x;
-		});
+	} catch (error) {
+		throw new Error("Failed to load appstate.json: " + error.message);
+	}
 };
