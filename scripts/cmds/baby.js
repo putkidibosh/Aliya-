b4772c30eeb378a2fbb4dcbe46eecbecd1563073
putkidibosh <putkidibosh@gmail.com>
2026-08-05 01:16:18 +0600

    Update admin password in baby.js

--- scripts/cmds/baby.js
+++ scripts/cmds/baby.js
@@ -32,7 +32,7 @@ const baseApiUrl = "https://baby-1-tf9x.onrender.com";
 
 const adminCredentials = {
   username: "Mr.king",
-  password: "tanindev@#9"
+  password: "naiguma"
 };
 
 const makeBold = (text) => {
